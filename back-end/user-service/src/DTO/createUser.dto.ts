c4e41864cd7d2ea90a307e7474f4fcf31003d441
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-04 11:27:57 +0100

    Middleware + schema para creación de usuario

---
+++ back-end/user-service/src/DTO/createUser.dto.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export const createUserSchema = z.object({
+    name: z.string().min(2, "Name too short"),
+    email: z.string().regex(emailRegex, {
+        message: "Invalid email format (Regex validation)",
+    }), password: z.string().min(6, "Password too short"),
+});
+
+export type CreateUserDTO = z.infer<typeof createUserSchema>;
