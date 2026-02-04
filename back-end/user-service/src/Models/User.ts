9236050ed8695d919b04af6e381cfe436be1f1cd
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-04 11:08:10 +0100

    Modelo con id opcional (para autogenerar).

--- back-end/user-service/src/Models/User.ts
+++ back-end/user-service/src/Models/User.ts
@@ -1,8 +1,11 @@
 export class User {
+  public id?: number;
   constructor(
-    public id: number,
     public nameApp: string,
     public email: string,
-    public password: string
-  ) {}
+    public password: string,
+    id?: number
+  ) {
+    if (id) this.id = id;
+  }
 }
