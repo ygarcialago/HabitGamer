9a229b5763e762b0625a28971c25e9a342e6048d
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-04 11:08:25 +0100

    Modificaciones para adaptarse al modelo

--- back-end/user-service/src/Repository/userRepository.ts
+++ back-end/user-service/src/Repository/userRepository.ts
@@ -3,14 +3,21 @@ import { User } from '../Models/User.js'
 
 export class UserRepository {
     async save(user: User): Promise<User> {
-    const created = await prisma.user.create({ data: user });
-    return new User(created.id, created.nameApp, created.email, created.password);
-  }
+        const created = await prisma.user.create({
+            data: {
+                nameApp: user.nameApp,
+                email: user.email,
+                password: user.password,
+            },
+        });
 
-  async findByEmail(email: string): Promise<User | null> {
-    const u = await prisma.user.findUnique({ where: { email } });
-    return u ? new User(u.id, u.nameApp, u.email, u.password) : null;
-  }
+        return new User(created.nameApp, created.email, created.password, created.id);
+    }
 
-  
+    async findByEmail(email: string): Promise<User | null> {
+        const u = await prisma.user.findUnique({ where: { email } });
+        if (!u) return null;
+
+        return new User(u.nameApp, u.email, u.password, u.id);
+    }
 }
