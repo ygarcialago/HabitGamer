762428b1fc525b7e15caa60210dd92fef30e51ee
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-05 12:33:56 +0100

    Entidad habit creada

---
+++ back-end/habit-service/src/Model/habit.ts
@@ -0,0 +1,38 @@
+import {
+    HabitKind,
+    HabitFrequency,
+    HabitImpact,
+} from "../Enum/habits.enum.js";
+
+
+export class Habit {
+    private constructor(
+        public readonly id: string | null,
+        public readonly userId: number,
+        public name: string,
+        public description: string | null,
+        public kind: HabitKind,
+        public frequency: HabitFrequency,
+        public impact: HabitImpact,
+        public value: number,
+        public isActive: boolean,
+        public readonly createdAt: Date | null,
+        public readonly updatedAt: Date | null,
+    ) { }
+
+    isGood(): boolean {
+        return this.kind === HabitKind.GOOD;
+    }
+
+    isBad(): boolean {
+        return this.kind === HabitKind.BAD;
+    }
+
+    affectsXP(): boolean {
+        return this.impact === HabitImpact.XP;
+    }
+
+    affectsLife(): boolean {
+        return this.impact === HabitImpact.LIFE;
+    }
+}
