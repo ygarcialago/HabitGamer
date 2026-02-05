ea6e3905ab9231ac4bb07b9133d0bf5865070f60
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-05 14:00:36 +0100

    Ruta definida para el save

--- back-end/habit-service/src/Route/routes.ts
+++ back-end/habit-service/src/Route/routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express"
+import { authenticateAndValidate } from "../Middleware/validateBodyAuth.js";
+import { createHabitSchema } from "../DTO/createHabit.dto.js";
+import { HabitController } from "../Controller/habitController.js";
 
 const route = Router();
+
+route.post("/createHabit", authenticateAndValidate(createHabitSchema), HabitController.save)
 
 export default route;
