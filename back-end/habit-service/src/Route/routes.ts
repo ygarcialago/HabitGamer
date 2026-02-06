5f72b7fd2863adc6d8a26cc1a816b5554801dc86
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-06 16:43:41 +0100

    Rutas declaradas (obtener hábitos + logs).

--- back-end/habit-service/src/Route/routes.ts
+++ back-end/habit-service/src/Route/routes.ts
@@ -3,11 +3,15 @@ import { authenticateAndValidate } from "../Middleware/validateBodyAuth.js";
 import { createHabitSchema } from "../DTO/createHabit.dto.js";
 import { changeActivenessHabitSchema } from "../DTO/changeActivenessHabit.dto.js";
 import { HabitController } from "../Controller/habitController.js";
+import { HabitLogController } from "../Controller/habitLogController.js";
 
 const route = Router();
 
 route.post("/createHabit", authenticateAndValidate(createHabitSchema), HabitController.save)
-
+route.get("/:userId", HabitController.getUserHabits);
 route.patch("/changeActivity", authenticateAndValidate(changeActivenessHabitSchema), HabitController.changeActiveness)
+
+route.post("/:habitId/mark", HabitLogController.markHabit)
+route.get("/:habitId/streak", HabitLogController.getStreak)
 
 export default route;
