c9bd03d93f59bd459ceac495d60fda79e8e555bc
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-10 19:39:00 +0100

    Punto de incio definido

---
+++ back-end/player-service/src/server.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import app from "./Config/config.js"
+import route from './Routes/routes.js'
+
+const port = process.env.PORT
+const commonURLEndpoint = '/player'
+
+app.listen(port, () => {
+    console.log(`Microservicio iniciado en el puerto ${port}`)
+})
+
+app.use(commonURLEndpoint, route)
