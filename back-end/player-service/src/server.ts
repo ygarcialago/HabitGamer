2897b3ab8c81e9d8944382517e88d677c4014935
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-11 16:49:58 +0100

    Cors instalado y habilitado para el front (dev local)

--- back-end/player-service/src/server.ts
+++ back-end/player-service/src/server.ts
@@ -1,9 +1,16 @@
 import 'dotenv/config'
 import app from "./Config/config.js"
 import route from './Routes/routes.js'
+import cors from "cors";
 
 const port = process.env.PORT
 const commonURLEndpoint = '/player'
+
+var corsOptions = {
+    origin: 'http://localhost:5173'
+}
+
+app.use(cors(corsOptions))
 
 app.listen(port, () => {
     console.log(`Microservicio iniciado en el puerto ${port}`)
