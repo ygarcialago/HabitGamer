fca6e57264857142663a805ea3371bbaa721ebd5
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-11 13:30:38 +0100

    Rutas definidas

--- back-end/player-service/src/Routes/routes.ts
+++ back-end/player-service/src/Routes/routes.ts
@@ -3,5 +3,7 @@ import { PlayerController } from "../Controller/playerController.js";
 
 const route = Router();
 route.post("/", PlayerController.create);
+route.patch("/takeDamage", PlayerController.takeDamage)
+route.patch("/gainXp", PlayerController.addExperience)
 
 export default route;
